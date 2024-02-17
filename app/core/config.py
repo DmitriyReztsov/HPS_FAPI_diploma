df7ec8dd03577078533589f2c4f0a7552c8164e5
DmitriyReztsov <rezcov_d@mail.ru>
2024-02-17 18:24:44 +0300

    console command Typer

--- app/core/config.py
+++ app/core/config.py
@@ -12,6 +12,10 @@ class Settings(BaseSettings):
     def ASYNC_DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     class Config:
         env_file = ".env"
 
